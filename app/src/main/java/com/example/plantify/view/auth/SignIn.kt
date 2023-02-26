a120cf0e834638fea798c7b33f251b8297408d50
intel <adityakhaladkar98@gmal.com>
2023-02-26 12:06:35 +0530

    migrated java code to kotlin

---
+++ app/src/main/java/com/example/plantify/view/auth/SignIn.kt
@@ -0,0 +1,12 @@
+package com.example.plantify.view.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plantify.R
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
